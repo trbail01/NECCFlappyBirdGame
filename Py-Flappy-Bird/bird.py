b315489105901e1ba7188fe715c1d3e29d56b61b
trbail01 <tbailey_123@hotmail.com>
2024-11-26 11:03:13 -0600

    Changed flight settings for jump_move.

--- Py-Flappy-Bird/bird.py
+++ Py-Flappy-Bird/bird.py
@@ -12,7 +12,7 @@ class Bird(pygame.sprite.Sprite):
         # bird basic info
         self.frame_index = 0
         self.animation_delay = 7 # WAS 3
-        self.jump_move = -9
+        self.jump_move = -9.5 # was -9
         # bird animation
         self.bird_img = import_sprite("assets/bird")
         self.image = self.bird_img[self.frame_index]
