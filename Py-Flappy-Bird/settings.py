d10f336c372a9a7b82238c135669d104655e71a8
trbail01 <tbailey_123@hotmail.com>
2024-11-26 16:52:37 -0600

    Changed some settings.py to make it easier. See comments in there.

--- Py-Flappy-Bird/settings.py
+++ Py-Flappy-Bird/settings.py
@@ -25,7 +25,7 @@ bottom_pipe_pair_sizes = [
 
 
 pipe_size = HEIGHT // 9 #was 10
-pipe_gap = (pipe_size * 2) + (pipe_size // 3); # was // 2
+pipe_gap = (pipe_size * 2) + (pipe_size // 2);
 ground_space = 50
 
 def import_sprite(path):
